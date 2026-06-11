1951826803840e7b0ca64f9d123b65ba8b68bb0c
mosalah2049 <mohammedsalah2049@gmail.com>
2026-06-11 14:28:18 +0530

    Allow create_admin.py to read superuser credentials from environment variables

--- create_admin.py
+++ create_admin.py
@@ -9,12 +9,12 @@ django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-username = 'admin'
-password = 'admin'
-email    = 'admin@eventhub.com'
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+email    = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@eventhub.com")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin")
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
-    print(f"Superuser created - username: {username}  password: {password}")
+    print(f"Superuser created - username: {username}")
 else:
     print(f"Superuser '{username}' already exists.")
